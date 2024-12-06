d6c642df88c458ea76d4cb132300f0ba1660d41f
OMAR <omar@localhost>
2024-12-06 18:57:46 +0100

    + custom form login simple

--- frontend/travelplanner-frontend-web/src/components/custom/Login.tsx
+++ frontend/travelplanner-frontend-web/src/components/custom/Login.tsx
@@ -1,5 +1,24 @@
+import Button from "./form-elements/Button";
+import { Input } from "./form-elements/Input";
+// Card
+// Title
+// input
+// input
+// forgot password
+// button
+// not a member
+
 export function Login() {
   return (
-    
+    <form>
+      <Input name="username" type="text"></Input>
+      <Input name="password" type="text"></Input>
+      <div>
+        <p>
+          Forgot password
+        </p>
+      </div>
+      <Button label="Sign in"/>
+    </form>
   )
 }
