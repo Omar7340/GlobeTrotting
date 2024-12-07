4777a0a17f7120d67bacadb36a386ba610979ffc
Omar <38433787+Omar7340@users.noreply.github.com>
2024-12-07 03:10:30 +0100

    + request login

--- frontend/travelplanner-frontend-web/src/components/custom/LoginForm.tsx
+++ frontend/travelplanner-frontend-web/src/components/custom/LoginForm.tsx
@@ -1,7 +1,27 @@
 import { Stack, Card, Button, Input } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field"
+import { MouseEventHandler, useState } from "react";
+import api from '@/misc/api.json'
 
 export function LoginForm() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const data = {
+    username : username,
+    password : password,
+  }
+
+  const handleSubmit : MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+    
+    fetch(api.host + 'users/login/', {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify(data)
+     }).then((e) => console.log(e));
+  }
+
   return (
     <Card.Root maxW="sm">
       <Card.Header>
@@ -10,16 +30,16 @@ export function LoginForm() {
       <Card.Body>
         <Stack gap="4" w="full">
           <Field label="Username">
-            <Input />
+            <Input value={username} onChange={(e) => setUsername(e.target.value)}/>
           </Field>
           <Field label="Password">
-            <Input />
+            <Input value={password} onChange={(e) => setPassword(e.target.value)}/>
           </Field>
         </Stack>
       </Card.Body>
       <Card.Footer justifyContent="flex-end">
         <Button variant="outline">Lost password?</Button>
-        <Button variant="solid">Sign in</Button>
+        <Button onClick={handleSubmit} variant="solid">Sign in</Button>
       </Card.Footer>
     </Card.Root>
   )
