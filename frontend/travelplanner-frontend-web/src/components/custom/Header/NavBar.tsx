52ef8530c333a2e5dd37bc11c51ffd6bde8fdaff
OMAR <omar@localhost>
2024-12-11 01:43:53 +0100

    ~ organize imports

--- frontend/travelplanner-frontend-web/src/components/custom/Header/NavBar.tsx
+++ frontend/travelplanner-frontend-web/src/components/custom/Header/NavBar.tsx
@@ -1,9 +1,9 @@
+import backgroundimg from '@/assets/cat-bg.png'
 import { useEffect, useState } from "react"
-import NavBarContainer from "./NavBarContainer"
-import Logo from "./Logo"
-import MenuLinks from "./MenuLinks"
-import MenuToggle from "./MenuToggle"
-import backgroundimg from '@/assets/cat-bg.png' 
+import Logo from './Logo'
+import MenuLinks from './MenuLinks'
+import MenuToggle from './MenuToggle'
+import NavBarContainer from './NavBarContainer'
 
 export default function NavBar ({...props}) {
   const [isOpen, setIsOpen] = useState(false)
